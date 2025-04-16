18f9a90f807c84411a469882eec318bc6ff20d72
Isaac Dickson <dicksonisaac101@gmail.com>
2025-04-16 06:40:08 +0000

    Watching the folder for changes

--- src/main/java/com/IsaacDickson/PIACProject/PiacProjectApplication.java
+++ src/main/java/com/IsaacDickson/PIACProject/PiacProjectApplication.java
@@ -2,8 +2,10 @@ package com.IsaacDickson.PIACProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PiacProjectApplication {
 
 	public static void main(String[] args) {
